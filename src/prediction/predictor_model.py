2509ebff2bb754cecf2145e41f11a168190f7ae7
moo3030 <mohamedabdelhamid3030@gmail.com>
2024-04-01 01:22:43 +0200

    create model directory from code

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -258,6 +258,7 @@ def predict_with_model(model: MoiraiPredictor, context: pd.DataFrame):
 
 
 def save_predictor_model(model: MoiraiPredictor, model_dir: str) -> None:
+    os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, "model.txt")
     with open(model_path, "w") as f:
         f.write("dummy model")
