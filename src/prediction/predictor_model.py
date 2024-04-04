19acee529db1a70d269e8f0f72669f3797b0a321
moo3030 <mohamedabdelhamid3030@gmail.com>
2024-04-04 14:17:37 +0200

    added support to covariates

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -2,7 +2,7 @@ import os
 from contextlib import contextmanager
 from copy import deepcopy
 from pathlib import Path
-from typing import Iterator, Optional, Union
+from typing import Iterator, Optional, Union, List
 import torch
 import torch.nn as nn
 import numpy as np
@@ -18,8 +18,7 @@ from gluonts.transform.split import TFTInstanceSplitter
 from uni2ts.model.moirai.forecast import MoiraiForecast
 from schema.data_schema import ForecastingSchema
 from prediction.download_model import download_pretrained_model_if_not_exists
-
-count_patches = {}
+from gluonts.dataset.common import ListDataset
 
 
 class CustomizableMoiraiForecast(MoiraiForecast):
@@ -49,6 +48,9 @@ class CustomizableMoiraiForecast(MoiraiForecast):
 
 
 class MoiraiPredictor(Predictor):
+    made_up_frequency = "S"  # by seconds
+    made_up_start_dt = "2000-01-01 00:00:00"
+
     def __init__(
         self,
         data_schema: ForecastingSchema,
@@ -59,6 +61,7 @@ class MoiraiPredictor(Predictor):
         num_samples: Optional[int] = 20,
         batch_size: Optional[int] = 16,
         context_length: Optional[int] = 50,
+        use_exogenous: Optional[bool] = True,
     ) -> None:
         super().__init__(prediction_length, lead_time=lead_time)
         self.data_schema = data_schema
@@ -66,6 +69,8 @@ class MoiraiPredictor(Predictor):
         self.batch_size = batch_size
         self.patch_size = patch_size
         self.context_length = context_length
+        self.use_exogenous = use_exogenous
+        self.freq = self.map_frequency(data_schema.frequency)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.forecast_generator = SampleForecastGenerator()
         self.prediction_net = prediction_net.to(self.device)
@@ -156,13 +161,11 @@ class MoiraiPredictor(Predictor):
             if self.patch_size is not None
             else self.prediction_net.hparams.patch_size
         )
-
         if self.patch_size == "auto_dataset":
             patch_size = self._get_best_patch_size(
                 dataset, self.batch_size, context_length, prediction_length
             )
-
-            count_patches[patch_size] = count_patches.get(patch_size, 0) + 1
+            print("Selected patch_size:", patch_size)
 
         with self.prediction_net.custom_config(
             context_length=context_length,
@@ -225,9 +228,147 @@ class MoiraiPredictor(Predictor):
         else:
             return 1
 
+    def prepare_training_data(
+        self,
+        history: pd.DataFrame,
+    ) -> List:
+        """
+        Applys the history_forecast_ratio parameter and puts the training data into the shape expected by GluonTS.
+
+        Args:
+            history (pd.DataFrame): The input dataset.
+
+        Returns (ListDataset): The processed dataset expected by GluonTS.
+        """
+        data_schema = self.data_schema
+        # Make sure there is a date column
+        history = self.prepare_time_column(data=history, is_train=True)
+
+        # Manage each series in the training data separately
+        all_covariates = []
+        groups_by_ids = history.groupby(data_schema.id_col)
+        all_ids = list(groups_by_ids.groups.keys())
+        all_series = [
+            groups_by_ids.get_group(id_).drop(columns=data_schema.id_col)
+            for id_ in all_ids
+        ]
+
+        fut_cov_names = []
+        past_cov_names = []
+        static_cov_names = []
+
+        if self.use_exogenous:
+            fut_cov_names = data_schema.future_covariates
+            past_cov_names = data_schema.past_covariates
+            static_cov_names = data_schema.static_covariates
+
+        # Put future covariates into separate list
+        all_covariates = []
+        static_covariates = []
+
+        for series in all_series:
+            series_past_covariates = []
+            series_future_covariates = []
+            series_static_covariates = []
+
+            for covariate in fut_cov_names:
+                series_future_covariates.append(series[covariate])
+
+            for covariate in past_cov_names:
+                series_past_covariates.append(series[covariate])
+
+            for covariate in static_cov_names:
+                series_static_covariates.append(series[covariate].iloc[0])
+
+            all_covariates.append((series_future_covariates, series_past_covariates))
+            static_covariates.append(series_static_covariates)
+
+        # If covariates are available for training, create a dataset with covariate features,
+        # otherwise a dataset with only target series will be created.
+
+        start = (
+            series[data_schema.time_col].iloc[0]
+            if data_schema.time_col_dtype not in ["INT", "OTHER"]
+            else "2020-01-01"
+        )
+        list_dataset = [
+            {
+                # "start": series[data_schema.time_col].iloc[0],
+                "start": pd.Period(
+                    start,
+                    self.map_frequency(data_schema.frequency),
+                ),
+                "target": series[data_schema.target],
+            }
+            for series in all_series
+        ]
+
+        if self.use_exogenous and fut_cov_names:
+            for item, cov_series in zip(list_dataset, all_covariates):
+                item["feat_dynamic_real"] = cov_series[0]
+
+        if self.use_exogenous and past_cov_names:
+            for item, cov_series in zip(list_dataset, all_covariates):
+                item["past_feat_dynamic_real"] = cov_series[1]
+
+        if self.use_exogenous and static_cov_names:
+            for item, cov_series in zip(list_dataset, static_covariates):
+                item["feat_static_real"] = cov_series
+
+        return list_dataset
+
+    def prepare_time_column(
+        self, data: pd.DataFrame, is_train: bool = True
+    ) -> pd.DataFrame:
+        """
+        Adds time column of type DATETIME to datasets that have time column dtype as INT.
+
+        Args:
+            data (pd.DataFrame): The input dataset.
+            is_train (bool): Set to true for training dataset and false for testing dataset.
+
+            Returns (pd.DataFrame): The dataset after processing time column.
+        """
+        # sort data
+        time_col_dtype = self.data_schema.time_col_dtype
+        id_col = self.data_schema.id_col
+        time_col = self.data_schema.time_col
+
+        data = data.sort_values(by=[id_col, time_col])
+
+        if time_col_dtype == "INT":
+            # Find the number of rows for each location (assuming all locations have
+            # the same number of rows)
+            series_val_counts = data[id_col].value_counts()
+            series_len = series_val_counts.iloc[0]
+            num_series = series_val_counts.shape[0]
+
+            if is_train:
+                # since GluonTS requires a date column, we will make up a timeline
+                start_date = pd.Timestamp(self.made_up_start_dt)
+                datetimes = pd.date_range(
+                    start=start_date, periods=series_len, freq=self.made_up_frequency
+                )
+                self.last_timestamp = datetimes[-1]
+                self.timedelta = datetimes[-1] - datetimes[-2]
+
+            else:
+                start_date = self.last_timestamp + self.timedelta
+                datetimes = pd.date_range(
+                    start=start_date, periods=series_len, freq=self.made_up_frequency
+                )
+            int_vals = sorted(data[time_col].unique().tolist())
+            self.time_to_int_map = dict(zip(datetimes, int_vals))
+            # Repeat the datetime range for each location
+            data[time_col] = list(datetimes) * num_series
+        else:
+            data[time_col] = pd.to_datetime(data[time_col])
+            data[time_col] = data[time_col].dt.tz_localize(None)
+
+        return data
+
 
 def train_predictor_model() -> None:
-
     return None
 
 
@@ -238,23 +379,12 @@ def predict_with_model(model: MoiraiPredictor, context: pd.DataFrame):
     all_series = [series for _, series in grouped]
 
     all_forecasts = []
-    for _, series in zip(all_ids, all_series):
-        start = (
-            series[schema.time_col].iloc[0]
-            if schema.time_col_dtype not in ["INT", "OTHER"]
-            else "2020-01-01"
-        )
+    data = model.prepare_training_data(history=context)
+    for i in data:
+        print(ListDataset([i], model.freq))
         forecast = list(
             model.predict(
-                dataset=[
-                    {
-                        "target": series[schema.target],
-                        "start": pd.Period(
-                            start,
-                            model.map_frequency(schema.frequency),
-                        ),
-                    }
-                ],
+                ListDataset([i], model.freq),
                 context_length=model.context_length,
             )
         )[0].samples
@@ -263,7 +393,7 @@ def predict_with_model(model: MoiraiPredictor, context: pd.DataFrame):
 
         all_forecasts.append(median_forecast)
     all_forecasts = np.array(all_forecasts)
-    print("Number of selected patch sizes:", count_patches)
+    print("all_forecasts:", all_forecasts.shape)
     return {k: v for k, v in zip(all_ids, all_forecasts)}
 
 
